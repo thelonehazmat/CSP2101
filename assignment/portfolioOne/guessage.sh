667e02c34e4ba83e2af7e8e09a1266b96bb3c3e4
BRANDON ROYCE <thelonehazmat@github.com>
2020-04-08 04:54:19 +0000

    Finall assignment

--- assignment/portfolioOne/guessage.sh
+++ assignment/portfolioOne/guessage.sh
@@ -3,7 +3,13 @@
 #Name: Brandon Royce
 #Student Number: Brandon Royce
 
-echo "Welcome to the guessing game. Guess the number or press q to quit"
+#Colours
+Clear='\033[0m'
+Red='\033[31m'
+Green='\033[32m'
+Yellow='\033[33m'
+
+echo -e "${Yellow}Welcome to the age guessing game. Guess the age (between 20 and 70) or press q to quit."
 
 generateNumber() #Function to generate random number when called
 {
@@ -15,18 +21,18 @@ generateNumber() #Function to generate random number when called
 test()
 {
     if  (($guess == $age)) ; then #If the guess is correct
-        echo "Success, you guessed the age"
+        echo -e "${Green}Success, you guessed the age"
         exit
     
     elif [ $guess == "q" ]; then #If the escape character is used
-        echo "Goodbye"
+        echo -e "${Yellow}Goodbye"
         exit
 
     elif (($guess > $age)); then #If the guess is too high
-        echo "Too high"
+        echo -e "${Red}Too high"
 
     elif (($guess < $age)); then #If the guess is too low
-        echo "Guess is too low"
+        echo -e "${Red}Guess is too low"
 
     fi
 }
@@ -36,13 +42,14 @@ age=$? #Reads random number into age
 
 while (true); do #Loops until broken by the correct number or q
     
+    echo ""
     read -p "Guess the number: " guess #Prompts user for input
     
     if [[ $guess =~ ^[0-9]+$ ]]; then #Uses regular expression to check if it's an integer
         test #Calls test function
 
     else #Tells user that their integer is wrong
-        echo "You have entered an invalid character, integers only"
+        echo -e "${Red}You have entered an invalid character, integers only"
 
     fi
 
