ad2d31ff371d0b4025b650579abe6840d8cacab1
BRANDON ROYCE <thelonehazmat@github.com>
2020-05-19 03:14:34 +0000

    Hey

--- assignment/portfolioTwo/rectangle.sh
+++ assignment/portfolioTwo/rectangle.sh
@@ -3,16 +3,18 @@
 #Brandon Royce
 #Student Number: 10471905
 
-#rm rectangle_F.txt
-initialFile=rectangle.txt
-#read -p "What file would you like to format?" initialFile
+
+read -p "What file would you like to format? " initialFile #Asks what file will be processed
+read -p "What file would you like to output to? " outputFile #Asks where the results will be stored
 
 sed -e '1d' \
 -e 's/Rec[0-9]/Name: &/' \
--e 's/,[0-9]/   Height: &/1' \
--e 's/,[0-9]/   Width: &/2' \
--e 's/,[0-9]/   Area: &/3' \
--e 's/,[A-Z][a-z]*\+/   Colour: &/' \
--e 's/,//g' $initialFile > rectangle_F.txt
+-e 's/,[0-9]/\tHeight: &/1' \
+-e 's/,[0-9]/\tWidth: &/2' \
+-e 's/,[0-9]/\tArea: &/3' \
+-e 's/,[A-Z][a-z]*\+/\tColour: &/' \
+-e 's/,//g' $initialFile > $outputFile #Saves results
+
+echo -e "Results succesfully stored in $outputFile\n"
 
 exit 0
