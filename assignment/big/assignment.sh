8f67b61daeed4866e3f64b8b8e25589dbdcbbc76
BRANDON ROYCE <thelonehazmat@github.com>
2020-05-31 04:44:19 +0000

    Assignment Done

--- assignment/big/assignment.sh
+++ assignment/big/assignment.sh
@@ -5,7 +5,7 @@
 
 site=https://www.ecu.edu.au/service-centres/MACSC/gallery/gallery.php?folder=152 #Stores image gallery address in a variable
 
-rm results.txt &> /dev/null #Removes old file and pipes error into null if it din't exist
+rm results.txt &> /dev/null #Removes old file and pipes error into null if it didn't exist
 curl -s $site > results.txt #Downloads Image gallery html page
 grep -Eo '(http|https)://[^"]+' results.txt | grep "DSC0" | sed -n 's/.*\///w results.txt' #Strips all text except for file names
 
@@ -15,12 +15,12 @@ inputCheck() { #Function for checking complicated input
     local exitStatus
     exitStatus=1
     userInput=$1
-    until (( exitStatus == 0 ))
+    until (( exitStatus == 0 )) #Ends loop once grep finds the file
     do
         
         if grep -q -Fx "DSC0$userInput.jpg" results.txt #Checks if user input is a valid file name
         then
-            exitStatus=0           
+            exitStatus=0     #If input is correct ends the loop      
         else
             exitStatus=1
             echo "Invalid input"
@@ -36,6 +36,7 @@ downloadOutput() { #Outputs a message of file size etc.
     file=$(echo $1 | grep -Eo "(1|2)[0-9]+") #Sanitises input
     size=$(curl -sI https://secure.ecu.edu.au/service-centres/MACSC/gallery/152/DSC0$file.jpg | grep -i Content-Length | tr -d '\r' | awk '{print $2/1000}') #Gets file size
     echo -e "\nDownloading DSC0$file, with the file name DSC0$file.jpg, with a file size of $size KB...File Download Complete\n" #Outputs result
+    
     
 }
 
@@ -60,11 +61,11 @@ rangeFiles(){ #Downloads a range of files
     local lower
     local upper
 
-    read -p "What is the start of the range you wish to download? " lower
+    read -p "What is the start of the range you wish to download (Last four digits of file)? " lower
     inputCheck $lower
     lower=$correctInput #Assigns lower file name
 
-    read -p "What is the end of the range you wish to download? " upper
+    read -p "What is the end of the range you wish to download (Last four digits of file)? " upper
     inputCheck $upper
     upper=$correctInput
 
@@ -82,6 +83,7 @@ randomFiles() { #Downloads a random number of files the user specifies
     do
         read -p "How many random images do you wish to download (Enter digits only)? " randomRange
     done
+
     for ((i=0; i<$randomRange; i++)) do #Loop through desired amount of images
     randomFile=$(shuf -n 1 results.txt) #Chooses a random file
         wget -q -P $storage "https://secure.ecu.edu.au/service-centres/MACSC/gallery/152/$randomFile"
@@ -90,33 +92,37 @@ randomFiles() { #Downloads a random number of files the user specifies
 }
 
 #Main body
-read -p "What directory would you like to save the files to? " storage #Saves where to save the files
+
+until [[ -n $storage ]] #Checks user input isn't null
+do
+    read -p "What directory would you like to save the files to? " storage #Saves where to save the files
+done
 
 until [[ $option = "x" ]]; do #Loops until user chooses to exit program
     echo -e "\nPress 1 to download a specified file \nPress 2 to download all files \nPress 3 to download a range of files \nPress 4 to download random files\nPress x to exit\n"
     read -p "What would you like to do? " option
 
     if [ $option = "1" ]; then
-        specificFile
+        specificFile #Calls specific file function
         echo -e "\nPROGRAM FINISHED"
 
     elif [ $option = "2" ]; then
-        allFiles
+        allFiles #Calls all files function
         echo -e "\nPROGRAM FINISHED"
 
     elif [ $option = "3" ]; then
-        rangeFiles
+        rangeFiles #Calls range files function
         echo -e "\nPROGRAM FINISHED"
 
     elif [ $option = "4" ]; then
-        randomFiles
+        randomFiles #Calls random files option
         echo -e "\nPROGRAM FINISHED"
 
     elif [ $option = "x" ]; then
         echo -e "\nPROGRAM FINISHED"
 
     else
-        echo "Please enter either 1, 2, 3, 4 or x only."
+        echo "Please enter either 1, 2, 3, 4 or x only." #Called if user input is invalid
 
     fi
 
